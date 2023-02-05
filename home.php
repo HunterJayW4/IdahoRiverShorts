25c04c21f21155eeb45a71d447e39cfb8b2bea7a
HunterJayW4 <hunterjayw@gmail.com>
2023-02-05 15:17:02 -0700

    Initial Commit

---
+++ home.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+ 
+<html>
+
+<link rel="stylesheet" href="Style.css">
+
+    <head>
+        <div id="Header">
+            <div id="HeaderRec">
+                    <img src= "Images/logoTemp2.png" alt="logo" id = "Logo">
+                    <img src= "Images/cart.png" alt="ShoppingCart" id = "Cart">
+            </div>
+        </div>
+        <hr>
+        <div id="Nav">
+            <ul class="nav">
+                <li><a href="home.php">HOME</a></li>
+                <li><a href="/">ABOUT</a></li>
+                <li><a href="/">SHOP</a></li>
+                <li><a href="/">CONTACT</a></li>
+            </ul>
+        </div>
+    </head>
+
+        <body>
+            <p>
+                At Idaho River Sports, we value quality over anything else. We sell only the highest quality fabrics
+                and garments. We have a deep rooted passion for all things shorts. It doesn't matter if it's a 10 inch
+                inseam, where the shorts are bordering on capris, or a 2 inch inseam risking ball exposure, we love it
+                all.  <br><br> "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum
+                deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident,
+                similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum
+                quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio
+                cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis
+                dolor repellendus.<br><br> Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe
+                eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a
+                sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus
+                asperiores repellat."
+            </p>
+
+
+
+        </body>
+</html>
