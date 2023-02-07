2bf6e6a354796ff981010be16dd97cc889d08b62
HunterJayW4 <hunterjayw@gmail.com>
2023-02-07 12:47:11 -0700

    Functionality of navigation links added.

--- home.php
+++ home.php
@@ -1,26 +1,11 @@
 <!DOCTYPE html>
- 
+<?php include("Header.php"); ?>
 <html>
-
+<title>Idaho River Shorts</title>
 <link rel="stylesheet" href="Style.css">
+<link rel="icon" type="image/png" href="/Images/shorts.png">
 
-    <head>
-        <div id="Header">
-            <div id="HeaderRec">
-                    <img src= "Images/logoTemp2.png" alt="logo" id = "Logo">
-                    <img src= "Images/cart.png" alt="ShoppingCart" id = "Cart">
-            </div>
-        </div>
-        <hr>
-        <div id="Nav">
-            <ul class="nav">
-                <li><a href="home.php">HOME</a></li>
-                <li><a href="/">ABOUT</a></li>
-                <li><a href="/">SHOP</a></li>
-                <li><a href="/">CONTACT</a></li>
-            </ul>
-        </div>
-    </head>
+
 
         <body>
             <p>
